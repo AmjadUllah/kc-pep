ec00d86e00edaeb366377d933b6c6ca9fa0de816
Amjad Ullah <amjad.qau@gmail.com>
2020-07-15 11:51:55 +0100

    feat(setup):setup function added

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import request, url_for, Flask, jsonify, flash
-from parameters import keycloak_server, keycloak_realm, client_id, client_secret, ssl_mode
+import parameters
+from parameters import keycloak_server, keycloak_realm, client_id, client_secret, ssl_mode, admin_id, admin_pwd
 import os
 import os.path
 import requests
@@ -8,6 +9,10 @@ import logging
 import json
 import sys
 import ssl
+
+import io 
+import csv
+from flask import make_response
 
 def __init__():
     global logger
@@ -24,6 +29,135 @@ def unhandle_request_error(error):
     response.status_code= 500
     return response
 
+@app.route('/setup', methods=['POST'])
+def setup():
+    csvList = [["user_name","pwd","device_id","scope"]]
+    si = io.StringIO()
+    cw = csv.writer(si)
+    
+    # Read parameters
+    input_json_body = request.json
+    entries = input_json_body ['entries']
+    user_prefix = input_json_body ['user_prefix']
+    role_name = input_json_body ['role_name']
+    start_index = input_json_body['start_index']
+    
+    # Response
+    response = dict(status_code="", message="", data=[])
+
+    # Get admin access token
+    admin_AT = get_admin_access_token()
+    if admin_AT == "":
+        response["message"] = "Unable to login admin user."
+        response["status_code"] = 400
+        response = jsonify(dict(message=str(error)))
+        response.status_code= 400
+        return response
+    # get role ID
+    role_id = get_role_id(admin_AT,role_name)
+    if role_id == "":
+        response["message"] = "Unable to obtain role_id."
+        response["status_code"] = 400
+        response = jsonify(dict(message=str(error)))
+        response.status_code= 400
+        return response
+    # Creat Users, Resources, Policies
+    for entry in range(entries):
+        index = entry + int(start_index)
+        userName, pwd = add_user(admin_AT,index,user_prefix,role_id)
+        if userName != "" and pwd != "":
+            user_id = get_user_id(admin_AT,userName)
+            logger.info("user_id => {0}".format(user_id))
+            assign_role_to_user(admin_AT,user_id,role_id,role_name)
+            tmpList=[userName,pwd,"deviceB","access"]
+            csvList.append(tmpList)
+    # Response
+    for row in csvList:
+        cw.writerow(row)
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
+def get_admin_access_token():
+    try:
+        kc_token_URL = keycloak_server + "realms/" + keycloak_realm + "/protocol/openid-connect/token"
+        grant_type = "password"
+        payload = {"grant_type":grant_type, "client_id":client_id, "client_secret": client_secret, "username":admin_id, "password":admin_pwd}
+        r = requests.post(kc_token_URL, data=payload)
+        logger.info(r.status_code)
+        response  = r.json()
+        access_token = response['access_token']
+        logger.info("access token: {0}".format(access_token))
+        return access_token
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+def get_role_id(access_token,role_name):
+        role_id = ""
+        role_api_url = keycloak_server + "admin/realms/" + keycloak_realm + "/roles/" + role_name 
+        headers = {'Authorization': 'Bearer ' + access_token}
+        r = requests.get(role_api_url,headers=headers)
+        logger.info("Get role id response. \n status_code => {0} \n response_message => {1}".format(r.status_code,r.text))
+        if r.status_code == 200:
+            result_json = r.json()
+            if "id" in result_json.keys():
+                role_id = result_json["id"]
+        return role_id
+
+def add_user(access_token,entry,user_prefix,role_id):
+    user_id = user_prefix + str(entry)
+    pwd = user_prefix + str(entry)
+    fName = user_prefix
+    lName = str(entry)
+    email = user_prefix + str(entry) + "@test.com"
+    try: 		
+        new_user_data = {"email": email,
+            "username": user_id,
+            "enabled": True,
+            "firstName": fName,
+            "lastName": lName,
+            "realmRoles": ["user_default", ],
+            "credentials": [{"value": pwd,"type": "password",}]
+        }
+        user_uri = keycloak_server + "admin/realms/" + keycloak_realm + "/users"
+        headers = {'Authorization': 'Bearer ' + access_token}
+        r = requests.post(user_uri,json=new_user_data,headers=headers)
+        logger.info("Status Code => {0} \n Text => {1}".format(r.status_code,r.text))
+        if r.status_code == 201:
+            return user_id,pwd
+        return "",""
+    except Exception as error:
+        logger.error(error)
+        raise error
+
+def get_user_id(access_token,user_name):
+        user_id = ""
+        users_api_url = keycloak_server + "admin/realms/" + keycloak_realm + "/users?username=" + user_name 
+        headers = {'Authorization': 'Bearer ' + access_token}
+        r = requests.get(users_api_url,headers=headers)
+        logger.info("Get user id response. \n status_code => {0} \n response_message => {1}".format(r.status_code,r.text))
+        if r.status_code == 200:
+            ret  = r.json()
+            if ret!=[]: 
+                user_id  = r.json()[0]['id']
+        return user_id
+
+def assign_role_to_user(access_token,user_id,role_id,role_name):   # assign role to user
+    try:
+        api_url = keycloak_server + "admin/realms/" + keycloak_realm + "/users/" + user_id + "/role-mappings/realm"
+        headers = {'Authorization': 'Bearer ' + access_token}
+        roles = [{"id": role_id, "name": role_name}]
+        r = requests.post(api_url,json=roles,headers=headers)
+        logger.info("Assign role response: \n status_code => {0} \n response_message => {1}".format(r.status_code,r.text))
+        if r.status_code == 204:
+            logger.info("Role assignment successful.")
+        else:
+            logger.error("Role assignment unsuccessful")
+    except Exception as e:
+        logger.error(e)
+        raise e
 
 @app.route('/access', methods=['POST'])
 def access():
--- parameters.py
+++ parameters.py
@@ -3,8 +3,10 @@ import logging
 
 """ Configurable parameters provided as enviornment variables """
 
-keycloak_server = os.getenv("KEYCLOAK_SERVER","http://localhost/auth/")
+keycloak_server = os.getenv("KEYCLOAK_SERVER","http://kc-server/auth/")
 keycloak_realm = os.getenv("KEYCOLAK_REALM","acFog")
 client_id = os.getenv("CLIENT_ID","smartFog")
 client_secret = os.getenv("CLIENT_SECRET","dea3ddd2-bca3-4b19-9881-a1637ae69b45")
 ssl_mode = os.getenv("SSL_MODE","0") # For (http = 0, https = 1, mTLS = 2)
+admin_id = os.getenv("ADMIN_ID","fogmanager")
+admin_pwd = os.getenv("ADMIN_PWD","fogmanager")
